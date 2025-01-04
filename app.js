a0afc77c4fc1d0d6b79c5296c3d8a1126f032691
vansh-29 <vansh74177@gmail.com>
2025-01-05 01:34:11 +0530

    port changed

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // user-vansh74177 , gEd2FhAGW0VwvuZV
 // mongodb+srv://vansh74177:gEd2FhAGW0VwvuZV@cluster0.t9j29.mongodb.net/
+require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 const path = require("path");
 var cors = require('cors')
 app.use(cors({
-    origin: 'http://localhost:3000', // Replace with your frontend URL
+    origin: '*', // Replace with your frontend URL
     methods: 'GET,POST,PUT,DELETE',
     credentials : true
   }));
@@ -27,10 +28,12 @@ app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 
-app.listen(1000 , ()=>{
-    console.log('server started')
-})
+const PORT = process.env.PORT || 1000; // Use the PORT environment variable or fall back to 1000 locally
 
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+  
 const authRouter = require('./routes/authRouter')
 app.use( '/auth' , authRouter)
 
